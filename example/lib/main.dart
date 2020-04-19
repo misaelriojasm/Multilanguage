8eeeedbd37bd65e838b1d9f3ff62b9d9f2741539
misaelriojas <misael.riojas@fulltimeforce.com>
2020-04-19 15:45:28 -0500

    better example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,56 +1,32 @@
-import 'package:flutter/material.dart';
-import 'dart:async';
-
-import 'package:flutter/services.dart';
 import 'package:multilanguage/multilanguage.dart';
+import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(new MyApp());
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  String _platformVersion = 'Unknown';
-
-  @override
-  void initState() {
-    super.initState();
-    initPlatformState();
-  }
-
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    try {
-      platformVersion = await Multilanguage.platformVersion;
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
-    }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-
-    setState(() {
-      _platformVersion = platformVersion;
-    });
-  }
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    /// SETS THE LANGUAGE INSIDE THE INITIAL APP LAUNCH
+    /// YOU COULD SET IT ANYWHERE TO CHANGE THE CURRENT LANGUAGE
+    MultiLanguage.setLanguage(path: Languages.en, context: context);
+
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Plugin example app'),
-        ),
-        body: Center(
-          child: Text('Running on: $_platformVersion\n'),
-        ),
+      routes: {
+        '/Home': (context) => HomeView(),
+      },
+    );
+  }
+}
+
+class HomeView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        /// YOU COULD USE TXT SHORTCUT TO SEARCH FOR THE TEXT
+        title: Text(txt('title')),
       ),
+      body: Container(),
     );
   }
 }
